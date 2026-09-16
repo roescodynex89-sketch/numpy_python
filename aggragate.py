9f2cd6bd9495c6593dd420377a28a48abeaa75e4
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-09-16 11:09:07 +0600

    argmax

---
+++ aggragate.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array([
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+])
+
+a.sum()
+a.mean()
+a.std()          # standard deviation
+a.min(), a.max()
+a.argmax()         # big value-এর index
