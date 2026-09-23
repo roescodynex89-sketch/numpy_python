6aea7b00ceebcba3b909cb7b4b015c8e6f657372
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-09-23 19:33:19 +0600

    1D array

--- reshaping.py
+++ reshaping.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-# 1. First, create a 1D array with 12 elements
+# 1. First, create a 1D array with 12 elements add 
+
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 print("Original array (1D):", arr)
 print("Shape of the original array:", arr.shape) # Output will be (12,)
